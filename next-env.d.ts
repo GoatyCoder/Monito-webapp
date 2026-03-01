cfd38bf91a62bb9b01dac1211d5b4a50d2e59042
GoatyCoder <78029345+GoatyCoder@users.noreply.github.com>
2026-03-01 20:45:19 +0100

    Revert "Implementa gestione anagrafiche con CRUD locale nella pagina /anagrafiche"

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
+// Questo file è generato/gestito da Next.js.
+// Non modificare manualmente la struttura di base.
